65cecff4c39bd03bd38984a346267bdaf58186bb
Stijn Verhoeven <stijn.verhoeven@outlook.be>
2019-10-05 21:11:44 +0200

    added keyvault references

--- AzureFunction/DeployACIContainer/run.ps1
+++ AzureFunction/DeployACIContainer/run.ps1
@@ -10,31 +10,69 @@ Write-Host "PowerShell HTTP trigger function processed a request."
 $rgName = $Request.Query.RGName
 $containergroupName = $Request.Query.ACIGroupName
 
+# check if VSTS_AGENT_INPUT_TOKEN and ACRPASSWORD is valid (keyvault reference used, does not work for local development)
+# if local development, use the Uri of the secret to get the secret value
+if ($env:VSTS_AGENT_INPUT_TOKEN -like "@Microsoft.KeyVault(SecretUri*")
+{
+    $secretId = $($env:VSTS_AGENT_INPUT_TOKEN).Substring($($env:VSTS_AGENT_INPUT_TOKEN).IndexOf('=')+1).Replace(')','')
+    $secretUri = [System.Uri]$secretId
+    $pattoken = (Get-AzKeyVaultSecret -VaultName $secretUri.Host.Split('.')[0] -Name $secretUri.Segments[2].Replace('/','')).SecretValueText
+}
+else
+{
+    $pattoken = $env:VSTS_AGENT_INPUT_TOKEN
+}
+
+if ($env:ACRPASSWORD -like "@Microsoft.KeyVault(SecretUri*")
+{
+    $secretId = $($env:ACRPASSWORD).Substring($($env:ACRPASSWORD).IndexOf('=')+1).Replace(')','')
+    $secretUri = [System.Uri]$secretId
+    $acrpassword = (Get-AzKeyVaultSecret -VaultName $secretUri.Host.Split('.')[0] -Name $secretUri.Segments[2].Replace('/','')).SecretValueText
+}
+else
+{
+    $acrpassword = $env:ACRPASSWORD
+}
 
 # create environmental variable hashtable
 $azdevopsagentVariables = @{
     "VSTS_AGENT_INPUT_URL" = $env:VSTS_AGENT_INPUT_URL;
     "VSTS_AGENT_INPUT_AUTH" = $env:VSTS_AGENT_INPUT_AUTH;
-    "VSTS_AGENT_INPUT_TOKEN" = $env:VSTS_AGENT_INPUT_TOKEN;
-    "VSTS_AGENT_INPUT_AGENT" = $env:VSTS_AGENT_INPUT_AGENT
+    "VSTS_AGENT_INPUT_TOKEN" = $pattoken;
+    "VSTS_AGENT_INPUT_AGENT" = $env:VSTS_AGENT_INPUT_AGENT;
+    "VSTS_AGENT_INPUT_POOL" = $env:VSTS_AGENT_INPUT_POOL
 }
 
 # create ps credential for access to acr
-$secpasswd = ConvertTo-SecureString $env:ACRPASSWORD -AsPlainText -Force
+$secpasswd = ConvertTo-SecureString $acrpassword -AsPlainText -Force
 $creds = New-Object System.Management.Automation.PSCredential ($env:ACRUSERNAME, $secpasswd)
 
 # create container group
-$acigroup = New-AzContainerGroup -ResourceGroupName $rgName `
+try
+{
+    $acigroup = New-AzContainerGroup -ResourceGroupName $rgName `
                 -Name $containergroupName `
                 -Image $env:IMAGENAME `
                 -EnvironmentVariable $azdevopsagentVariables `
                 -OsType Linux `
-                -RegistryCredential $creds
+                -RegistryCredential $creds `
+                -ErrorAction Stop
 
-$body = "container group $containergroupName has been deployed"
-$status = [HttpStatusCode]::OK
-# Associate values to output bindings by calling 'Push-OutputBinding'.
-Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
-    StatusCode = $status
-    Body = $body
-})
+    $body = "container group $containergroupName has been deployed"
+    $status = [HttpStatusCode]::OK
+    # Associate values to output bindings by calling 'Push-OutputBinding'.
+    Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+        StatusCode = $status
+        Body = $body
+    })   
+}
+catch
+{
+    $body = "Error occured during container deployment: $_"
+    $status = [HttpStatusCode]::InternalServerError
+    # Associate values to output bindings by calling 'Push-OutputBinding'.
+    Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+        StatusCode = $status
+        Body = $body
+    })
+}
