b5d0735cd02acc3ebb100ca7364636079f709a07
Stijn Verhoeven <stijn.verhoeven@outlook.be>
2019-10-22 15:51:21 +0200

    fixed function app pipeline

--- AzureFunction/StartStopACIContainerGroup/run.ps1
+++ AzureFunction/StartStopACIContainerGroup/run.ps1
@@ -9,61 +9,65 @@ Write-Host "PowerShell HTTP trigger function processed a request."
 # Interact with query parameters or the body of the request.
 $action = $Request.Query.Action
 $rgName = $Request.Query.RGName
-$containergroupName = $Request.Query.ACIGroupName
 
-if (-not $rgName -or -not $containergroupName -or -not $action)
+if (-not $rgName -or -not $action)
 {
     $rgName = $Request.Body.RGName
-    $containergroupName = $Request.Body.ACIGroupName
     $action = $Request.Body.Action
 }
 
 try
 {
-    switch ($action) {
-        "start" { 
-            if ((Get-AzContainerGroup -ResourceGroupName $rgName -Name $containergroupName).State -eq "Running")
-            {
-                $status = [HttpStatusCode]::OK
-                $body = @{status="already started"} | ConvertTo-Json
+    $ACIs = Get-AzContainerGroup -ResourceGroupName $rgName
+
+    foreach ($ACI in $ACIs)
+    {
+        switch ($action) {
+            "start" { 
+                if ((Get-AzContainerGroup -ResourceGroupName $rgName -Name $ACI.Name).State -eq "Running")
+                {
+                    $status = [HttpStatusCode]::OK
+                    $body = @{status="already started"} | ConvertTo-Json
+                }
+                else
+                {
+                    $returnstatus = Invoke-AzResourceAction -ResourceGroupName $rgName `
+                                        -ResourceName $ACI.Name `
+                                        -Action $action `
+                                        -ResourceType Microsoft.ContainerInstance/containerGroups `
+                                        -Force
+                   
+                    $status = [HttpStatusCode]::OK
+                    $body = @{status="start successful"} | ConvertTo-Json
+                }
+             }
+        
+             "stop" {
+                if ((Get-AzContainerGroup -ResourceGroupName $rgName -Name $ACI.Name).State -eq "Stopped")
+                {
+                    $status = [HttpStatusCode]::OK
+                    $body = "$($ACI.Name) already stopped"
+                }
+                else
+                {
+                    $returnstatus = Invoke-AzResourceAction -ResourceGroupName $rgName `
+                                        -ResourceName $ACI.Name `
+                                        -Action $action `
+                                        -ResourceType Microsoft.ContainerInstance/containerGroups `
+                                        -Force
+        
+                    $status = [HttpStatusCode]::OK
+                    
+                    $body = @{status="stop successful"} | ConvertTo-Json
+                }
+             }
+            Default {
+                $status = [HttpStatusCode]::BadRequest
+                $body = @{status="error"} | ConvertTo-Json
             }
-            else
-            {
-                $returnstatus = Invoke-AzResourceAction -ResourceGroupName $rgName `
-                                    -ResourceName $containergroupName `
-                                    -Action $action `
-                                    -ResourceType Microsoft.ContainerInstance/containerGroups `
-                                    -Force
-               
-                $status = [HttpStatusCode]::OK
-                $body = @{status="start successful"} | ConvertTo-Json
-            }
-         }
-    
-         "stop" {
-            if ((Get-AzContainerGroup -ResourceGroupName $rgName -Name $containergroupName).State -eq "Stopped")
-            {
-                $status = [HttpStatusCode]::OK
-                $body = "$containergroupName already stopped"
-            }
-            else
-            {
-                $returnstatus = Invoke-AzResourceAction -ResourceGroupName $rgName `
-                                    -ResourceName $containergroupName `
-                                    -Action $action `
-                                    -ResourceType Microsoft.ContainerInstance/containerGroups `
-                                    -Force
-    
-                $status = [HttpStatusCode]::OK
-                
-                $body = @{status="stop successful"} | ConvertTo-Json
-            }
-         }
-        Default {
-            $status = [HttpStatusCode]::BadRequest
-            $body = @{status="error"} | ConvertTo-Json
         }
     }
+    
     
     # Associate values to output bindings by calling 'Push-OutputBinding'.
     Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
